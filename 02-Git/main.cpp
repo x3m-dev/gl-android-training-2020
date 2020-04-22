84c08da790e8799a22b50bbb1ffa92c2b939a9a8
Maxim Sagaydachny <win.drivers@gmail.com>
2020-04-22 07:41:35 +0300

    02-Git: Init project

---
+++ 02-Git/main.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Rock / Paper / Scissors gambling game (single player - human vs computer) @ Bot Inc." << std::endl;
+
+    return 0;
+}
